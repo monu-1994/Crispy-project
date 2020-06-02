fea74242270715ff6d9bbb002e3d4ef82444754a
komal shambharkar <komalshambharkarkomal94@gmail.com>
2020-06-02 16:29:35 +0530

    veggie

---
+++ add_product.php
@@ -0,0 +1,123 @@
+
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Veggie Restaurant Category Flat Bootstrap Responsive Website Template | Contact :: W3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Veggie Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+  
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+	
+	<!-- css files -->
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' /><!-- bootstrap css -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' /><!-- custom css -->
+    <link href="css/font-awesome.min.css" rel="stylesheet"><!-- fontawesome css -->
+	<!-- //css files -->
+	
+	<!-- google fonts -->
+	<link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+	<!-- //google fonts -->
+	
+</head>
+<body>
+
+<!-- top header -->
+<div class="header-top">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<ul class="d-lg-flex header-w3_pvt">
+					<li class="mr-lg-3">
+						<span class="fa fa-envelope-open"></span>
+						<a href="mailto:info@example.com" class="">info@example.com</a>
+					</li>
+					<li>
+						<span class="fa fa-phone"></span>
+						<p class="d-inline">Call Us +12 345 678</p>
+					</li>
+				</ul>
+			</div>
+			<div class="col-sm-6 header-right-w3_pvt">
+				<ul class="d-lg-flex header-w3_pvt justify-content-lg-end">
+					<li class="">
+						<span class=""><span class="fas fa-clock"></span>Mon - Sun : 8:30am to 9:30pm</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- //top header -->
+
+<!-- //header -->
+<header class="py-sm-3 pt-3 pb-2">
+	<div class="container">
+			<div id="logo">
+				<h1> <a href="index.html"><span class="fa fa-cutlery" aria-hidden="true"></span>Veggie</a></h1>
+			</div>
+		<!-- nav -->
+		<nav class="d-lg-flex">
+
+			<label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
+			<input type="checkbox" id="drop" />
+			<ul class="menu mt-2 ml-auto">
+				
+                <li class=""><a href="log out.html">Logout</a></li>
+                
+			</ul>
+			
+		</nav>
+		<div class="clear"></div>
+		<!-- //nav -->
+	</div>
+</header>
+<!-- //header -->
+
+<!-- banner -->
+<div class="innerpage-banner" id="home">
+	<div class="inner-page-layer">
+	</div>
+</div>
+<!-- //banner -->
+
+<!-- contact -->
+<section class="mail pt-lg-5 pt-4">
+	<div class="container pt-lg-5">
+		<h2 class="heading text-center mb-sm-5 mb-4">Add Recipe</h2>
+		<div class="row agileinfo_mail_grids">
+			<div class="col-lg-8 agileinfo_mail_grid_right">
+				  <form action="#" method="post">
+					  <button>Add recipe</button><br><br>
+					<div class="form-input">
+						
+					<ul>
+                        Title&nbsp&nbsp:<input type="text"><br><br>
+                        Recipe Description&nbsp&nbsp:<input type="text"><br><br>
+                        Price&nbsp&nbsp:<input type="text"><br><br>
+                        Images&nbsp&nbsp:<input type="file"><br><br>
+						</ul><br><br>
+                   	</div>
+							
+							<div class="ADD-buttons">
+								<button type="ADD" class="btn">ADD</button>
+							</div>
+	</body>
+	</html>
